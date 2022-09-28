87f7232a893b72a9a49d651dfeff4a29557e80fd
AndyAYu <andyhkgw@gmail.com>
2022-09-28 12:48:35 -0500

    nav bar through bootstrap?

--- solo_mern/src/components/ShowBookList.js
+++ solo_mern/src/components/ShowBookList.js
@@ -8,7 +8,12 @@ class ShowBookList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      classes: [],
+      sets: [],
+      types: [],
+      factions: [],
+      qualities: [],
+      races: [],
     };
   }
 
@@ -23,26 +28,26 @@ class ShowBookList extends Component {
   //   }
   // };
 
-  // axios.request(options).then(function (response) {
-  //   console.log(response.data);
-  // }).catch(function (error) {
-  //   console.error(error);
-  // });
+  // axios.request(options)
+  // .then(function (response){console.log(response.data);})
+  // .catch(function (error) {console.error(error);});
   };
 
 
   render() {
     const books = this.state.books;
     console.log("PrintBook: " + books);
-    let bookList;
+    let cardList;
+    let buttonList;
 
     if(!books) {
-      bookList = "there is no book record!";
+      cardList = "there is no book record!";
     } else {
-      bookList = books.map((book, k) =>
+      cardList = books.map((book, k) =>
         <BookCard book={book} key={k} />
       );
     }
+    buttonList = (Object.keys(this.state))
 
     return (
       <div className="ShowBookList">
@@ -57,6 +62,28 @@ class ShowBookList extends Component {
               <Link to="/create-book" className="btn btn-outline-warning float-right">
                 + Add New Book
               </Link>
+              <nav class="navbar navbar-expand-lg navbar-light bg-darkgrey">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
               <br />
               <br />
               <hr />
@@ -65,7 +92,7 @@ class ShowBookList extends Component {
           </div>
 
           <div className="list">
-                {bookList}
+                {cardList}
           </div>
         </div>
       </div>
