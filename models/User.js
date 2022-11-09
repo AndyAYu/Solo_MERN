8e7bc272e425b20a425cec4c6a0dfebef0267e9a
AndyAYu <andyhkgw@gmail.com>
2022-11-08 23:49:54 -0600

    userjs

--- models/User.js
+++ models/User.js
@@ -1,61 +1,31 @@
 //models/User.js
 
-import mongoose from 'mongoose'
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [ true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        index: true
+    },
     email: {
         type: String,
         lowercase: true,
-        trim: true,
         unique: true,
-        required: [true, "Email is required"],
-        match: [/^[a-zA-Z0-9]+$/, 'Email is invalid'],
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
         index: true
     },
-    hashed_password: {
+    password: {
         type: String,
-        required: [true, "Password is required"]
+        required: true
     },
-    salt: String
+    
 },{timestamps: true});
 
-UserSchema
-    .virtual('password')
-    .set(function(password) {
-        this._password = password
-        this.salt = this.makeSalt()
-        this.hashed_password = this.encryptPassword(password)
-    })
-    .get(function() {
-        return this._password
-    })
 
-UserSchema.methods = {
-    authenticate: function(plainText) {
-        return this.encryptPassword(plainText) === this.hashed_password
-    },
-    encryptPassword: function(password) {
-        if (!password) return ''
-        try {
-            return crypto
-                .createHmac('shal', this.salt)
-                .update(password)
-                .digest('hex')
-        } catch (err) {
-            return ''
-        }
-    },
-    makeSalt: function() {
-        return Math.round((new Date().valueOf() * Math.random())) + ''
-    }
-}
 
-UserSchema.path('hashed_password').validate(function(v) {
-    if (this._password && this._password.length < 6) {
-        this.invalidate('password', 'Password must be at least 6 characters.')
-    }
-    if (this.isNew && !this._password) {
-        this.invalidate('password', 'Password is required')
-    }
-},null)
-export default mongoose.model('User', UserSchema);
+module.exports = User = mongoose.model('User', UserSchema);
