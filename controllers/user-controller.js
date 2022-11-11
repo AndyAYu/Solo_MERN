b811cd1d07e2a02675487b542419e2ac55ee1819
AndyAYu <andyhkgw@gmail.com>
2022-11-11 02:24:08 -0600

    email validation error handling

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,9 +2,19 @@ const User = require("../models/User");
 
 
 const signup = async(req, res, next) => {
+    const {email,password} = req.body; //destructure req.body
+    let existingUser;
+    try {
+    existingUser = await User.find({ email: email})
+    } catch (err) {
+        console.log(err);
+    }
+    if (existingUser) {
+        return res.status(400).json({message: "Email already exists"})
+    }
     const user = new User({
-        email: req.body.email,
-        password: req.body.password
+        email, // email: email // req.body.email
+        password 
     });
 
     try {
