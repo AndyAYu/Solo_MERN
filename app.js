5b0e293a1f74233d861de72f515ebaa6fbf37e02
AndyAYu <andyhkgw@gmail.com>
2022-11-11 01:34:35 -0600

    refactored app.js

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
-// app.js
-
 const express = require('express');
-const connectDB = require('./config/db');
-// Connect Database
-connectDB();
-var cors = require('cors');
+const mongoose = require('mongoose');
 
-
-
-// routes
-const books = require('./routes/api/books');
-// const users = require('./routes/api/users');
 const app = express();
+
+mongoose
+    .connect(
+        "mongodb+srv://solo_mern:Solo5502!@cluster0.4z2ybt2.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => {
+        app.listen(8082);
+        console.log("MongoDB is connected! Listening to localhost 8082")
+    })
+    .catch((err) => console.log(err));
 
 
 // cors
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+// app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
 // use books
-app.use('/api/books', books);
+// app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// app.listen(port, () => console.log(`Server running on port ${port}`));
