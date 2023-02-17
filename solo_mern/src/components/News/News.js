d83b391b92df8d6c72d813361803495f7a93d7cb
AndyAYu <andyhkgw@gmail.com>
2023-02-16 18:03:48 -0600

    weird work done

--- solo_mern/src/components/News/News.js
+++ solo_mern/src/components/News/News.js
@@ -1,14 +1,18 @@
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('695d50b87ff846f6afff0a3e53aa2b1b');
+// const NewsAPI = require('newsapi');
+// const newsapi = new NewsAPI('695d50b87ff846f6afff0a3e53aa2b1b');
+// // To query /v2/top-headlines
+// // All options passed to topHeadlines are optional, but you need to include at least one of them
 
-export default function newsFetch () {
-    newsapi.v2.topHeadlines({
-    sources: 'bbc-news, the-verge',
-    q: 'software, crypto',
-    category: 'tech',
-    language: 'en',
-    country: 'us'
-}).then(response => {
-    console.log(response);
-})
-}
+// export default function News() {
+//     newsapi.v2.topHeadlines({
+//         sources: 'bbc-news,the-verge',
+//         q: 'bitcoin',
+//         category: 'business',
+//         language: 'en',
+//         country: 'us'
+//     }).then(response => {
+//         console.log(response);
+//         return response;
+//     });
+// };
+
--- solo_mern/src/components/ShowBookList.js
+++ solo_mern/src/components/ShowBookList.js
@@ -1,9 +1,10 @@
 import React, { Component, useState } from 'react';
 import '../App.css';
-// import axios from 'axios';
 import { Link } from 'react-router-dom';
 import BookCard from './BookCard';
-import newsFetch from './News/News';
+import NewsAPI from './News/News.js';
+import FlippableCard from './Card.js';
+
 
 const apikey = "695d50b87ff846f6afff0a3e53aa2b1b"
 
@@ -13,32 +14,22 @@ class ShowBookList extends Component {
     this.state = {
       news: {},
     };
+    this.newsFetch = this.newsFetch.bind(this);
   }
 
   componentDidMount() {
   };
 
+// fetch news api
 newsFetch() {
-  // fetch('https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=695d50b87ff846f6afff0a3e53aa2b1b')
+  fetch('https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=695d50b87ff846f6afff0a3e53aa2b1b')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => this.setState({ news: data }))
+    .catch(err => console.log(err));
+    return this.state.news;
 } 
 
   render() {
-    const books = this.state.books;
-    console.log("PrintBook: " + books);
-    let cardList;
-    // let buttonList;
-
-    if(!books) {
-      cardList = "there is no book record!";
-    } else {
-      cardList = books.map((book, k) =>
-        <BookCard book={book} key={k} />
-      );
-    }
-    // buttonList = (Object.keys(this.state))
-
     return (
       <div className="ShowBookList">
         <div className="container">
@@ -58,24 +49,24 @@ newsFetch() {
               <Link to="/login" className="btn btn-outline-info float-right">
                 Login
               </Link>
-              <nav class="navbar navbar-expand-lg navbar-light bg-darkgrey">
-                <a class="navbar-brand" href="#">Home</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
+              <nav className="navbar navbar-expand-lg navbar-light bg-darkgrey">
+                <a className="navbar-brand" href="#">Home</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="mentors">Decks <span class="sr-only">(current)</span></a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                  <ul className="navbar-nav">
+                    <li className="nav-item active">
+                      <a className="nav-link" href="mentors">Decks <span className="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Features</a>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Features</a>
                     </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Pricing</a>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Pricing</a>
                     </li>
-                    <li class="nav-item">
-                      <a class="nav-link disabled" href="#">Disabled</a>
+                    <li className="nav-item">
+                      <a className="nav-link disabled" href="#">Disabled</a>
                     </li>
                   </ul>
                 </div>
@@ -88,7 +79,7 @@ newsFetch() {
           </div>
 
           <div className="list">
-                {cardList}
+            <FlippableCard />
           </div>
         </div>
       </div>
