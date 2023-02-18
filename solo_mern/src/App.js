3b3ca7f7e6ffd6124d023c1b1197b77ff85deddd
AndyAYu <andyhkgw@gmail.com>
2023-02-18 00:00:21 -0600

    scrolltotop button works

--- solo_mern/src/App.js
+++ solo_mern/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import CreateBook from './components/CreateBook';
-import ShowBookList from './components/ShowBookList';
+import MainSplash from './components/MainSplash';
 import ShowBookDetails from './components/ShowBookDetails';
 import UpdateBookInfo from './components/UpdateBookInfo';
-import Mentors from './components/Mentors';
+import GoogleMaps from './components/GoogleMaps';
 import Login from './components/Login';
 import Signup from './components/Signup';
 
@@ -15,13 +15,13 @@ class App extends Component {
     return (
       <Router>
         <Routes>
-          <Route exact path='/' element={<ShowBookList/>} />
+          <Route exact path='/' element={<MainSplash/>} />
           <Route path='/login' element={<Login/>} />
           <Route path='/signup' element={<Signup/>} />
           <Route path='/create-book' element={<CreateBook/>} />
           <Route path='/edit-book/:id' element={<UpdateBookInfo/>} />
           <Route path='/show-book/:id' element={<ShowBookDetails/>} />
-          <Route path='/mentors' element={<Mentors/>} />
+          <Route path='/Google Maps' element={<GoogleMaps/>} />
         </Routes>
       </Router>
     );
