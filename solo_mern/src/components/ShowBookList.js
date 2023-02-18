6816ae3e953f95cab1bf3a6d62b1295b3bd11800
AndyAYu <andyhkgw@gmail.com>
2023-02-17 23:26:46 -0600

    nav bars edit making google maps api page

--- solo_mern/src/components/ShowBookList.js
+++ solo_mern/src/components/ShowBookList.js
@@ -44,11 +44,11 @@ class ShowBookList extends Component {
           <div className="row">
             <div className="col-md-12">
               <br />
-              <a href="localhost:3000/#"><h6 className="display-4 text-center text-light">The| | Co| |ector</h6></a>
+              <a href="localhost:3000/"><h6 className="display-4 text-center text-light">The| | Co| |ector</h6></a>
             </div>
 
             <div className="col-md-11">
-              <Link to="/create-book" className="btn btn-outline-warning float-right">
+              {/* <Link to="/create-book" className="btn btn-outline-warning float-right">
                 + Add New Book
               </Link>
               <Link to="/signup" className="btn btn-outline-info float-right">
@@ -56,7 +56,7 @@ class ShowBookList extends Component {
               </Link>
               <Link to="/login" className="btn btn-outline-info float-right">
                 Login
-              </Link>
+              </Link> */}
               <nav className="navbar navbar-expand-lg navbar-light bg-darkgrey">
                 <a className="navbar-brand" href="#">Home</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -65,7 +65,7 @@ class ShowBookList extends Component {
                 <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav">
                     <li className="nav-item active">
-                      <a className="nav-link" href="mentors">Decks <span className="sr-only">(current)</span></a>
+                      <a className="nav-link" href="google-maps">Google Maps <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
                       <a className="nav-link" href="#">Features</a>
@@ -90,12 +90,12 @@ class ShowBookList extends Component {
             {/* from news.articles => news['newsJson'].articles */}
             {this.state.news['newsJson'].articles && this.state.news['newsJson'].articles.map((article, i) => (
               <div className="card" key={i}>
-                <div className="card-body">
+                <a className="card-body hover-overlay"  target="_blank" href={article.url}>
                   <img className="img-responsive" width="100%"src={article.urlToImage}></img>
-                  <h5 className="text-uppercase w-100 h-50 text-dark">{article.title}</h5>
-                  <h6 className="text-dark text-sm">{article.author}</h6>
+                  <h5 className="w-100 h-50 text-dark">{article.title}</h5>
+                  <p className="text-dark">{article.author}</p>
                   <p className="text-dark">{article.description}</p>
-                </div>
+                </a>
               </div>
             ))}
           </div>
